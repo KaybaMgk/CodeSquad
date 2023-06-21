60ae99925f0dac8581309959142b773c834a360c
Ivette <nobiltaivette@gmail.com>
2023-06-21 11:32:20 -0300

    organizado de archivos y resolución de conflictos

--- conexion.py
+++ conexion.py
@@ -1,7 +1,10 @@
 import mysql.connector
-#creando clase
+from funciones import funciones
+# creando clase
+
+
 class db_leyes():
-    def __init__(self,host,user,password,port ,database,auth_plugin='mysql_native_password'):
+    def __init__(self, host, user, password, port, database, auth_plugin='mysql_native_password'):
         self.host = host
         self.user = user
         self.password = password
@@ -9,38 +12,29 @@ class db_leyes():
         self.database = database
         self.connection = None
         self.auth_plugin = auth_plugin
+
     def connect(self):
         try:
             self.connection = mysql.connector.connect(
                 host=self.host,
                 user=self.user,
                 password=self.password,
-                port=self.port, 
+                port=self.port,
                 database=self.database,
                 auth_plugin=self.auth_plugin
-                
+
             )
             print("Conexión exitosa a la base de datos.")
         except mysql.connector.Error as error:
 
             print("No se pudo establecer la conexión: {}".format(error))
 
-    def close(self):  
+    def close(self):
         if self.connection:
             self.connection.close()
             print("Conexión cerrada.")
 
-conexion = db_leyes(
-    host = 'localhost',
-    user = 'root',
-    password = 'root',
-    port = '3306',
-    database = 'proyecto'
-    )
-
-class funciones():
-
-    def listarLeyes(self):
+        def listarLeyes(self):
         if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
@@ -55,7 +49,8 @@ class funciones():
             try:
                 cursor = self.conexion.cursor()
                 sql = "INSERT INTO ley (id_leyes, nro_leyes, fecha, descripcion, categoria, jurisdiccion, or_legislativo, palabra_clave) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}','{5}', '{6}', '{7}')"
-                cursor.execute(sql.format(ley[0], ley[1], ley[2], ley[3], ley[4], ley[5], ley[6], ley[7]))
+                cursor.execute(sql.format(
+                    ley[0], ley[1], ley[2], ley[3], ley[4], ley[5], ley[6], ley[7]))
                 self.conexion.commit()
                 print("¡Ley registrada!\n")
             except mysql.connector.Error as ex:
@@ -66,7 +61,8 @@ class funciones():
             try:
                 cursor = ley.conexion.cursor()
                 sql = "UPDATE ley SET fecha = '{2}',, descripcion = '{3}', categoria = '{4}', jurisdiccion = '{5}', or_legislativo = '{6}', palabra_clave = '{7}' WHERE id_leyes = '{0}'"
-                cursor.execute(sql.format(ley[0], ley[1], ley[2], ley[3], ley[4], ley[5], ley[6], ley[7]))
+                cursor.execute(sql.format(
+                    ley[0], ley[1], ley[2], ley[3], ley[4], ley[5], ley[6], ley[7]))
                 self.conexion.commit()
                 print("Ley actualizada!\n")
             except mysql.connector.Error as ex:
@@ -83,61 +79,17 @@ class funciones():
             except mysql.connector.Error as ex:
                 print("Error al intentar la conexión: {0}".format(ex))
 
-conexion.connect()
-
-#Aqui es donde escribiran el codigo para que se ejecuten en mysql
-
-conexion.close()
-
-=======
-import mysql.connector
-#creando clase
-class db_leyes():
-    def __init__(self,host,user,password,port ,database,auth_plugin='mysql_native_password'):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.port = port
-        self.database = database
-        self.connection = None
-        self.auth_plugin = auth_plugin
-    def connect(self):
-        try:
-            self.connection = mysql.connector.connect(
-                host=self.host,
-                user=self.user,
-                password=self.password,
-                port=self.port, 
-                database=self.database,
-                auth_plugin=self.auth_plugin
-                
-            )
-            print("Conexión exitosa a la base de datos.")
-        except mysql.connector.Error as error:
-
-            print("No se pudo establecer la conexión: {}".format(error))
-
-    def close(self):  
-        if self.connection:
-            self.connection.close()
-            print("Conexión cerrada.")
 
 conexion = db_leyes(
-    host = 'localhost',
-    user = 'root',
-    password = 'pon tu contraseña',
-    port = '3306',
-    database = 'proyecto'
-
+    host='localhost',
+    user='root',
+    password='root',
+    port='3306',
+    database='proyecto'
 )
-
-#Crear un metodo o sea un def para llenar los valores de la leyes
-
-
 
 conexion.connect()
 
-#Aqui es donde escribiran el codigo para que se ejecuten en mysql
-
+funciones()
 
 conexion.close()
